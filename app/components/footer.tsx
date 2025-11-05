6bd0cc4213708ff8aa7b797827e0a25e1410af54
caowens <caowens555@yahoo.com>
2025-11-04 22:39:49 -0800

    Add footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div className="border-black/10 border-t h-[40px] flex items-center justify-center px-5 mt-10">
+            <div className="text-sm text-black/50">
+                &copy; 2024 My Next.js Blog
+            </div>
+        </div>
+    );
+};
