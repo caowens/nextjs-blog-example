4896388ab54dd399654c2c03e83ce72c4edf14d7
caowens <caowens555@yahoo.com>
2025-11-05 21:00:39 -0800

    Add logo image

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
     return (
         <div className="border-black/10 border-b h-[40px] flex items-center justify-between px-5">
-            <div>Logo</div>
+            <div>
+                <Image
+                    src="/tyler-the-creator-profile.jpg"
+                    alt="Logo"
+                    width={30}
+                    height={30}
+                    className="rounded-full"
+                    priority
+                />
+            </div>
 
             <ul className="flex items-center gap-3">
                 <li>
